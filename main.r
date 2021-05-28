5fa72f0fff0f7ece78d757d669766997b0f8b165
Ewelina Maria Drelich <e.drelich@stud.elka.pw.edu.pl>
2021-05-28 23:51:46 +0200

    one vs all

--- main.r
+++ main.r
@@ -9,17 +9,19 @@ DATA <- DATA_SET[-c(1), -c(1)]
 x <- apply(as.matrix(DATA[,-c(1)]), 2, as.numeric)
 # x <- DATA[,-c(1)]
 y <- as.integer(data.matrix(DATA[,c(1)]))
-
-y[y==2]=-1
+y[c(1:5)]=3
 
 #to trzeba poprawic rzeby wybiera³o losowo jeszcze na razie dzia³ajmy na ca³ym x
-n_train  <-  round(0.8*nrow(x))
-x_train  <-  x[c(0:n_train),]
-x_test   <-  x[-c(0:n_train),]
-y_train  <-  y[c(0:n_train)]
-y_test   <-  y[-c(0:n_train)]
+#n_train  <-  round(0.8*nrow(x))
+#x_train  <-  x[c(0:n_train),]
+#x_test   <-  x[-c(0:n_train),]
+#y_train  <-  y[c(0:n_train)]
+#y_test   <-  y[-c(0:n_train)]
 
-RANK = SVMRFE(x, y, CRITERIA)
+#RANK = SVMRFE(x, y, CRITERIA)
+
+
+
 
 
 
