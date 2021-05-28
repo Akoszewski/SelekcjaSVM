1b9c820f75c4e6f0f1321278199fa4a62be4bbfc
Ewelina Maria Drelich <e.drelich@stud.elka.pw.edu.pl>
2021-05-29 00:08:23 +0200

    one vs all

--- main.r
+++ main.r
@@ -1,5 +1,6 @@
 source("functions.r")
 
+
 DATA_SET <- read.csv("Datasets/Peptidome1/Peptidome1.txt", sep="\t", header=FALSE)
 DATA_SET <- as.data.frame(t(as.matrix(DATA_SET)))
 CRITERIA <- DATA_SET[c(1),-c(1:2)]
@@ -10,6 +11,8 @@ x <- apply(as.matrix(DATA[,-c(1)]), 2, as.numeric)
 # x <- DATA[,-c(1)]
 y <- as.integer(data.matrix(DATA[,c(1)]))
 y[c(1:5)]=3
+
+
 
 #to trzeba poprawic rzeby wybiera³o losowo jeszcze na razie dzia³ajmy na ca³ym x
 #n_train  <-  round(0.8*nrow(x))
