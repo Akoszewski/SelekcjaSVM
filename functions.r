e819a5e416f97fc2a9841c085c98dd5c9b985933
Ewelina Maria Drelich <e.drelich@stud.elka.pw.edu.pl>
2021-05-29 00:09:51 +0200

    one vs all2

--- functions.r
+++ functions.r
@@ -2,25 +2,39 @@ library(e1071)
 
 SVMRFE <- function(x, y, CRITERIA) {
     
-    RANK <- data.frame(c(1:(length(CRITERIA))))
+    RRANK <- data.frame(c(1:(length(CRITERIA))))
     p <- ncol(CRITERIA)
     
     x_svm = x
     CRITERIA_SVM = CRITERIA
+    y_svm = y
+    
+    
     while (p >= 2) {
-        model <- svm(x_svm, y, kernel = "linear", scale=FALSE)
-        beta = drop(t(model$coefs)%*%x_svm[model$index,])
-        beta0 = model$rho
         
-        rank_criteria <- beta^2
-        min_index <- which.min(rank_criteria)
+        rank_criteria = matrix(0, 1, ncol(CRITERIA_SVM))
+        
+        for (i in seq(y[which.max(y)])){ 
+            
+            y_svm[y==i] = 1
+            y_svm[y!=i] = -1
+            
+            model <- svm(x_svm, y_svm, kernel = "linear", scale=FALSE)
+            beta = drop(t(model$coefs)%*%x_svm[model$index,])
+            rank_criteria = rank_criteria+beta^2
+            
+        }
+        
+        rank_criteria_average = rank_criteria/y[which.max(y)]
+        
+        min_index <- which.min(rank_criteria_average)
         
         RANK[p, 1] <- CRITERIA_SVM[min_index]
         CRITERIA_SVM = CRITERIA_SVM[,-c(min_index)]
         x_svm <- x_svm[,-c(min_index)]
         p <- p - 1
-        
     }
+    
     RANK[1, 1] <- CRITERIA_SVM[1]
     
     return (RANK)
@@ -40,12 +54,11 @@ FScoreSelection <- function(x, y, CRITERIA) {
             bestIndices <- c(bestIndices, col)
         }
     }
-    #print(length(CRITERIA))
+    # print(length(CRITERIA))
     # print(length(fscores))
     # df <- data.frame(CRITERIA, fscores)
     # print(order(fscores))
     hist(fscores)
     hist(bestFscores)
-    print(CRITERIA[bestIndices])
     return (CRITERIA[bestIndices])
 }
