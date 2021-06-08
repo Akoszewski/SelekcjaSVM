60abe8f3525fe73054b630a34c724902fcb02612
Akoszewski <akoszews@gmail.com>
2021-06-08 23:33:14 +0200

    GetAccForBestFeatures

--- functions.r
+++ functions.r
@@ -31,7 +31,7 @@ FScoreSelection <- function(x, y, CRITERIA) {
     bestFscores <- c()
     bestIndices <- c()
     # RANK <- data.frame(c(1:(length(CRITERIA))))
-    for(col in 1:ncol(x)) {
+    for (col in 1:ncol(x)) {
         x1 = x[which(y == 1), col]
         x0 = x[which(y == 2), col]
         fscore = abs(mean(x0) - mean(x1))/sqrt(var(x0) + var(x1))
@@ -41,4 +41,41 @@ FScoreSelection <- function(x, y, CRITERIA) {
     df <- df[order(-fscores),]
     colnames(df) <- c('Fscores', 'Criteria')
     return (df)
+}
+
+GetPrediction <- function(x, y, splitProportion = 0.6) {
+    n_train  <-  round(splitProportion * nrow(x))
+    x_train  <-  x[c(0:n_train),]
+    x_test   <-  x[-c(0:n_train),]
+    y_train  <-  y[c(0:n_train)]
+    y_test   <-  y[-c(0:n_train)]
+    model <- svm(x_train, y_train, kernel = "linear", scale=FALSE)
+    # plot.svm(x_train, y_train, formula, fill = TRUE, grid = 50, slice = list(),symbolPalette = palette(), svSymbol = "x", dataSymbol = "o", ...)
+    pred <- predict(model, x_test)
+    df <- data.frame(pred, y_test)
+    colnames(df) <- c('Predicted', 'Actual')
+    return (df)
+}
+
+GetAccuracy <- function(prediction) {
+    correct_vals <- 0
+    for (row in 1:nrow(prediction)) {
+        if (round(prediction[row, 'Predicted']) == prediction[row, 'Actual']) {
+            correct_vals <- correct_vals + 1
+        }
+    }
+    return (correct_vals/nrow(prediction))
+}
+
+GetAccForBestFeatures <- function(x, y, noOfFeatures) {
+    bestFeatures <- fscores[1:noOfFeatures,]
+    DATA_F <- DATA[bestFeatures$Criteria]
+
+    x <- apply(as.matrix(DATA_F[,-c(1)]), 2, as.numeric)
+
+    prediction <- GetPrediction(x, y)
+    accuracy <- GetAccuracy(prediction)
+    # print(prediction)
+    print(paste("Accuracy (for", noOfFeatures, "features):", gsub(" ", "", paste(accuracy * 100, "%"))))
+    return (accuracy)
 }
